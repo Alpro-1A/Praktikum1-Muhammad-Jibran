845db884c31e819d3e5bfd904ef82f7c416d8fb8
Jibran <mjibrantariq246@outlook.com>
2023-10-13 12:43:59 +0700

    menambahkan main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "================================================================" <<endl<<endl;
+    cout << "Nama : Muhammad Jibran Tariq"<<endl<<endl;
+    cout << "NPM : 2310631170103 \n\n" ;
+    cout << "Kelas : A - Informatika \n\n";
+    cout << "Cita-cita  : DevOps Engineer \n\n";
+    cout << "Motivasi Masuk Informatika: Karena menyukai bidang ini sejak lama \n\n";
+    cout << "================================================================" <<endl<<endl;
+
+}
