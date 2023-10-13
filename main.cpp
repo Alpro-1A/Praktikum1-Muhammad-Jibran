f4508de32add1b602bbc2b82614a8e34a9fc5f90
Muhammad Jibran <mjibrantariq246@gmail.com>
2023-10-13 12:57:15 +0700

    memperbaiki main.cpp

--- main.cpp
+++ main.cpp
@@ -4,12 +4,12 @@ using namespace std;
 
 int main()
 {
-    cout << "================================================================" <<endl<<endl;
+    cout << "==================================================================" <<endl<<endl;
     cout << "Nama : Muhammad Jibran Tariq"<<endl<<endl;
     cout << "NPM : 2310631170103 \n\n" ;
     cout << "Kelas : A - Informatika \n\n";
     cout << "Cita-cita  : DevOps Engineer \n\n";
     cout << "Motivasi Masuk Informatika: Karena menyukai bidang ini sejak lama \n\n";
-    cout << "================================================================" <<endl<<endl;
+    cout << "=================================================================" <<endl<<endl;
 
 }
